d5c9defdc623868646200953e60b01ee1fb2af3d
RashedulHaqueRasel1 <rashedulhaquerasel1@gmail.com>
2025-08-17 23:55:00 +0600

    Gallerys CTA Update

--- src/components/Features/Gallerys/Gallerys.tsx
+++ src/components/Features/Gallerys/Gallerys.tsx
@@ -1,5 +1,4 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+
 import React from "react";
 import GalleryHeroSection from "./GalleryHeroSection/GalleryHeroSection";
 import GalleryStats from "./GalleryStats/GalleryStats";
@@ -19,36 +18,7 @@ export default function Gallerys() {
       <GalleryCategories />
 
       {/* Virtual Tour CTA */}
-      <section className="py-20 bg-blue-900 text-white">
-        <div className="container mx-auto px-4 text-center">
-          <div className="max-w-3xl mx-auto space-y-8">
-            <h2 className="text-3xl md:text-4xl font-bold">
-              Want to See More?
-            </h2>
-            <p className="text-xl text-blue-100">
-              Visit our campus to experience the vibrant learning environment
-              firsthand
-            </p>
-            <div className="flex flex-col sm:flex-row gap-4 justify-center">
-              <Button
-                asChild
-                size="lg"
-                className="bg-yellow-500 hover:bg-yellow-400 text-black"
-              >
-                <Link href="/contact">Schedule a Visit</Link>
-              </Button>
-              <Button
-                asChild
-                variant="outline"
-                size="lg"
-                className="border-white text-white hover:bg-white hover:text-blue-900 bg-transparent"
-              >
-                <Link href="/about">Learn More About Us</Link>
-              </Button>
-            </div>
-          </div>
-        </div>
-      </section>
+
 
       <CallToActionSection
         title="Want to See More?"
